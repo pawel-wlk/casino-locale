f36c047c06208a6aafe524b0f47ab301c894cf68
Bartosz Barnaba Rajczyk <bartorkrajczyk@gmail.com>
2020-05-04 14:22:26 +0200

    base page style and login page layout

--- casinolocale/settings.py
+++ casinolocale/settings.py
@@ -115,6 +115,10 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static")
+]
+
 LOGIN_REDIRECT_URL = "/"
 LOGOUT_REDIRECT_URL = "/login/"
 EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
