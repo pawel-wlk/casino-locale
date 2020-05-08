c6de122e40aa1a004771e1dff04b6f55ea518d1e
b3lg4r4th <karol@kobialka.me>
2020-05-09 00:39:53 +0200

    actual room list

--- game/views.py
+++ game/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-
+from . import consumers 
 
 @login_required(login_url='/login')
 def index(request):
-    return render(request, 'game/index.html')
+    return render(request, 'game/index.html', {
+        'rooms': consumers.current_games.items
+    })
 
 
 @login_required(login_url='/login')
