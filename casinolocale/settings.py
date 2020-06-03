7049fe120b242cabdb8f22a11af11cd50a542e6f
Paweł Wilkosz <pwilkosz5@gmail.com>
2020-06-04 00:03:27 +0200

    heroku deployment

--- casinolocale/settings.py
+++ casinolocale/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "z_f2adh8(!ehtxi9jo3=!pd%xpjf9wm@ok6_n1243$rk4t0#19"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['intense-bastion-96881.herokuapp.com']
 
 
 # Application definition
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -115,6 +116,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+ 
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static")
